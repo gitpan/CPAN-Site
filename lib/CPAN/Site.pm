d7a31ce2d65b308d8c5554a1a21cee315d280810
Mark Overmeer <mark@overmeer.net>
2011-06-03 22:19:43 +0000

    Import of MARKOV/CPAN-Site-1.07 from CPAN.
    gitpan-cpan-distribution: CPAN-Site
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         MARKOV/CPAN-Site-1.07.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/CPAN/Site.pm
+++ lib/CPAN/Site.pm
@@ -1,13 +1,13 @@
-# Copyrights 1998,2005-2010 by Mark Overmeer.
+# Copyrights 1998,2005-2011 by Mark Overmeer.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 1.06.
+# Pod stripped from pm file by OODoc 2.00.
 use warnings;
 use strict;
 
 package CPAN::Site;
 use vars '$VERSION';
-$VERSION = '1.06';
+$VERSION = '1.07';
 
 
 1;
